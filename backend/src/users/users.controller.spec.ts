3a2ee899e99592dc5c65556d18a045e3078e1c19
Prince Igwe <igwep297@gmail.com>
2022-08-20 05:16:44 +0100

    user unit test failed

--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -1,13 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import {UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockUsersService = {}
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      providers: [UsersService]
+    }).overrideProvider(UsersService)
+      .useValue(mockUsersService)
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
   });
