68b6048fc63b2538b0840a19718c52a3f7333b4a
Prince Igwe <igwep297@gmail.com>
2022-08-20 22:50:52 +0100

    updated Dockerfile, docker-compose-test.yml to run test in watch mode. Ran first successful test on the user controller

--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -2,17 +2,29 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import {UsersService } from './users.service';
 
+var ObjectID = require('mongodb').ObjectID;
+var objectId = new ObjectID();
+
 describe('UsersController', () => {
   let controller: UsersController;
 
-  const mockUsersService = {}
+  const mockUsersService = {
+    createUser:  jest.fn((email: string, password: string) => {
+      return {
+        email: email,
+	      password: password,
+	      _id: "1234567",
+	      __v: 0
+      }
+    })
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [UsersService]
     }).overrideProvider(UsersService)
-      .useValue(mockUsersService)
+      .useValue(mockUsersService) // todo: custom provider (check Testing documentation)
       .compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -21,4 +33,15 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create a User', async () => {
+    expect(await controller.createUser({email: "testuser@gmail.com", password: "testpass123"})).toEqual(
+      {
+        email: "testuser@gmail.com",
+	      password: "testpass123",
+	      _id: "1234567",
+	      __v: 0
+      }
+    )
+  })
 });
