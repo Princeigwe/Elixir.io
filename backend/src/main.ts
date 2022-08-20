fc22d43f4a0e9affeb6f2f97dd2186e391a1dd8d
Prince Igwe <igwep297@gmail.com>
2022-08-20 04:48:15 +0100

    installed class-calidator and class-transformer packages

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ValidationPipe} from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //removes unnecessary properties in POST request body
+      transform: true
+    })
+  )
   await app.listen(3000);
 }
 bootstrap();
