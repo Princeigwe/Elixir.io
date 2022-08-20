cf18cd2575f0cd7a10866b5d9094da05c3e0b8ca
Prince Igwe <igwep297@gmail.com>
2022-08-20 03:20:22 +0100

    modified the Dockerfile of backend folder to have working directory of backend, created user entity file and build the new backend image

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
+import {MongooseModule} from '@nestjs/mongoose'
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_DATABASE_URI),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
