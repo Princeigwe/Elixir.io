0082941d84376c388e6c0a4874a81761bfbe3200
Prince Igwe <igwep297@gmail.com>
2022-08-21 20:49:28 +0100

    added method to reject user to be created if email already exists

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -8,34 +8,55 @@ export class UsersService {
     constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
     /**
-     * It creates a new user with the given email and password, and then saves it to the database
-     * @param {string} email - string - This is the email of the user.
-     * @param {string} password - string - This is the password that the user will use to login.
-     * @returns A promise.
+     * It creates a new user in the database if the email doesn't already exist
+     * @param {string} email - string, password: string
+     * @param {string} password - string - the password that the user will use to login
+     * @returns The user object is being returned.
      */
     async createUser(email: string, password: string) {
+        const existingUser = await this.userModel.findOne({ email: email}).exec()
+        if (existingUser) { 
+            throw new HttpException('User with this email already exists', HttpStatus.BAD_REQUEST) 
+        }
         const user = new this.userModel({email: email, password: password});
         return user.save();
     }
 
+    /**
+     * It returns a list of users from the database
+     * @returns An array of users
+     */
     async getUsers() {
         const users =  await this.userModel.find().exec()
         if(!users.length) {throw new NotFoundException("Users Not Found")}
         return users
     }
 
+    /**
+     * It returns a user object if the user exists, otherwise it throws an error
+     * @param {string} email - string - This is the email of the user we want to find.
+     * @returns A user object
+     */
     async getUserByEmail(email: string) {
         const user = await this.userModel.findOne({"email": email}).exec()
         if(!user) { throw new NotFoundException("User Not Found") }
         return user
     }
 
+    /**
+     * It returns a user object if the user exists, otherwise it throws an error
+     * @param {any} _id - any
+     * @returns The user object
+     */
     async getUserByID(_id: any) {
         const user = await this.userModel.findOne({"_id": _id}).exec()
         if(!user) { throw new NotFoundException("User Not Found") }
         return user
     }
 
+    /**
+     * It deletes all users from the database
+     */
     async deleteUsers() {
         await this.userModel.deleteMany().exec()
     }
@@ -49,6 +70,10 @@ export class UsersService {
         await this.userModel.deleteOne({"email": email}).exec()
     }
 
+    /**
+     * This function deletes a user from the database by their ID
+     * @param {any} _id - The id of the user to be deleted
+     */
     async deleteUserByID(_id: any) {
         await this.userModel.deleteOne({"_id": _id}).exec()
     }
