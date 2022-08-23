67f7fe169c6cc51b08af6c1e44e3ca119543f083
Prince Igwe <igwep297@gmail.com>
2022-08-23 23:31:20 +0100

    started the auth feature

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_DATABASE_URI),
-    UsersModule
+    UsersModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
