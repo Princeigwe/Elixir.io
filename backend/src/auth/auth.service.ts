eb4ec16dcd4fc4287b1a9011226cf865510f8406
Prince Igwe <igwep297@gmail.com>
2022-08-24 02:30:19 +0100

    encrypting and decrypting user password with crypto

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,36 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus} from '@nestjs/common';
 import {UsersService} from '../users/users.service'
 
-import { createCipheriv, randomBytes, scrypt } from 'crypto'
+import { createCipheriv, randomBytes, createDecipheriv, scrypt } from 'crypto'
 import { promisify } from 'util';
 
-// steps to encrypt the password
+// variables responsible for encrypting password during user registration
+
 const initVector = randomBytes(16); // used to generate 16 bytes of random data
-const password = "user password"
 const secretKey = randomBytes(32); // used to generate 32 bytes of random data as secret key
 const algorithm = 'aes-256-cbc' // the algorithm for encrypting the data
-
-const cipher = createCipheriv(algorithm, secretKey, initVector) // the cipher function
-let encryptedPassword = cipher.update(password, "utf-8", "hex"); // encrypting the password
-encryptedPassword += cipher.final("hex"); // stopping the encryption process
-
 @Injectable()
 export class AuthService {
     constructor(private userService: UsersService) {}
 
     async registerUser(email: string, password: string) {
         // steps to encrypt the password
-        const initVector = randomBytes(16); // used to generate 16 bytes of random data
-        const secretKey = randomBytes(32); // used to generate 32 bytes of random data as secret key
-        const algorithm = 'aes-256-cbc' // the algorithm for encrypting the data
-
         const cipher = createCipheriv(algorithm, secretKey, initVector) // the cipher function
         let encryptedPassword = cipher.update(password, "utf-8", "hex"); // encrypting the password
         encryptedPassword += cipher.final("hex"); // stopping the encryption process
-
+        
         return this.userService.createUser(email, encryptedPassword)
     }
 
 
-    async validateUser() {}
+
+    async getValidatedUser(email: string, password: string) {
+        const user = await this.userService.getUserByEmail(email)
+        let encryptedPassword = user.password
+
+        // to check if the login password is decrypted encryptedPassword
+        const decipher = createDecipheriv(algorithm, secretKey, initVector); // decipher function
+        let decryptedPassword = decipher.update(encryptedPassword, "hex", "utf-8") //decrypting password
+        decryptedPassword += decipher.final("utf8"); // stopping decryption process
+
+        if (decryptedPassword == password) {
+            return user
+        }else{
+            throw new HttpException('User with this email already exists', HttpStatus.BAD_REQUEST) 
+        }
+
+    }
 }
