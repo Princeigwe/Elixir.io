9fd64c703871ed323df9fd5c95fb42143ea4ca07
Prince Igwe <igwep297@gmail.com>
2022-08-25 02:18:16 +0100

    defining tests passed

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,13 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import {AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
+  const mockAuthService = {
+
+    registerUser: jest.fn(() => (
+      {
+        "email": "testuser3@gmail.com",
+	      "password": "$2b$10$RkL9RZeAfcLuJZ7FjcLAGOmbV9qEJ6dTaR.v8hkbCzkvcEmU3Qsmi",
+	      "_id": "6306bf09da10b393973a01ea",
+	      "__v": 0
+      }
+    ))
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-    }).compile();
+      providers: [AuthService]
+    }).overrideProvider(AuthService)
+      .useValue(mockAuthService)
+      .compile();
 
     controller = module.get<AuthController>(AuthController);
   });
@@ -15,4 +31,5 @@ describe('AuthController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
 });
--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import {JwtService} from '@nestjs/jwt'
+import {UsersService} from '../users/users.service'
+
+
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  let mockJwtService = {}
+  let mockUsersService = {}
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: mockJwtService
+        },
+        {
+          provide: UsersService,
+          useValue: mockUsersService
+        }
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
