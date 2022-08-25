d667c3d22bad42d5ccdb87454bd1b7bc70433b9c
Prince Igwe <igwep297@gmail.com>
2022-08-25 18:57:15 +0100

    added test for admin registrateion

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -11,10 +11,22 @@ describe('AuthController', () => {
       {
         "email": "testuser3@gmail.com",
 	      "password": "testpass123",
+        "role": "User",
 	      "_id": "123456",
 	      "__v": 0
       }
+    )),
+
+    registerAdmin: jest.fn((email: string, password: string) => (
+      {
+        "email": "testuser1@gmail.com",
+	      "password": "testpass123",
+	      "_id": "1234567",
+        "role": "Admin",
+	      "__v": 0
+      }
     ))
+
   }
 
   beforeEach(async () => {
@@ -37,8 +49,20 @@ describe('AuthController', () => {
       "email": "testuser3@gmail.com",
       "password": "testpass123",
       "_id": "123456",
+      "role": "User",
       "__v": 0
     }
     expect(await controller.registerUser({email: 'testuser3@gmail.com', password: 'testpass123'})).toEqual(user)
+  })
+
+  it('should return a registered admin', async () => {
+    const user = {
+      "email": "testuser1@gmail.com",
+      "password": "testpass123",
+      "_id": "1234567",
+      "role": "Admin",
+      "__v": 0
+    }
+    expect(await controller.registerAdmin({email: 'testuser1@gmail.com', password: 'testpass123'})).toEqual(user)
   })
 });
