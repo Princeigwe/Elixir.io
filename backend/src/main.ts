649bfca6c445e82fa4617f9ca775e286e8bb464b
Prince Igwe <igwep297@gmail.com>
2022-08-25 16:42:45 +0100

    enabled CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
