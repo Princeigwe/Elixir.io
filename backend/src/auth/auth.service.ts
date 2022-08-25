bbdd72a5290f9a5752b18dff7e43d6d9f8611174
Prince Igwe <igwep297@gmail.com>
2022-08-25 01:40:47 +0100

    added comments to auth methods

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,5 @@
 import { Injectable, HttpException, HttpStatus} from '@nestjs/common';
 import {UsersService} from '../users/users.service'
-
-import { createCipheriv, randomBytes, createDecipheriv, scrypt } from 'crypto'
 import {JwtService} from '@nestjs/jwt'
 import * as bcrypt from 'bcrypt';
 
@@ -13,6 +11,13 @@ export class AuthService {
     ) {}
 
 
+    /**
+     * The function takes in an email and password, generates a salt, hashes the password with the
+     * salt, and then creates a user with the email and hashed password
+     * @param {string} email - string, password: string
+     * @param {string} password - The password that the user is trying to register with.
+     * @returns The userService.createUser method is being returned.
+     */
     async registerUser(email: string, password: string) {
         // steps to hashing user password 
         const salt = await bcrypt.genSalt(10) // generate salt
@@ -22,6 +27,17 @@ export class AuthService {
 
 
 
+  /**
+   * We're going to get a user by email, then we're going to compare the password that was passed in
+   * with the hashed password that we got from the database. If they match, we'll return the user. If
+   * they don't match, we'll throw an error
+   * @param {string} email - the email of the user
+   * @param {string} password - The password that the user entered in the login form.
+   * @returns {
+   *         "statusCode": 400,
+   *         "message": "Invalid Credentials"
+   *     }
+   */
     async getValidatedUser(email: string, password: string) {
         const user = await this.userService.getUserByEmail(email)
 
@@ -36,6 +52,12 @@ export class AuthService {
 
     }
 
+    /**
+     * It takes a userId, creates a payload with the userId, signs the payload with the jwtService, and
+     * returns the token in a cookie
+     * @param {any} userId - any - this is the userId that is returned from the database.
+     * @returns A cookie with the JWT token.
+     */
     async putJwtInCookieOnLogin(userId: any) {
         const payload = { userId: userId }
         const token = this.jwtService.sign(payload)
