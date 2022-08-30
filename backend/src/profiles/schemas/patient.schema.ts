144c8ae3f222cae04c171a8c83907e9c21a01685
Prince Igwe <igwep297@gmail.com>
2022-08-30 06:13:17 +0100

    changed medication to prescription

--- backend/src/profiles/schemas/patient.schema.ts
+++ backend/src/profiles/schemas/patient.schema.ts
@@ -39,7 +39,7 @@ export class Patient {
     maritalStatus: MaritalStatus
 
     @Prop({ type: mongoose.Schema.Types.Array })
-    medications: [
+    prescriptions: [
         // {
         //     drug: string,
         //     dosage: string,
