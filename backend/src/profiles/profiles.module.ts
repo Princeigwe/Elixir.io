8b6698fca9a8cedac264cb30329af3c82ba87cb1
Prince Igwe <igwep297@gmail.com>
2022-08-31 04:40:17 +0100

    registered doctor schema

--- backend/src/profiles/profiles.module.ts
+++ backend/src/profiles/profiles.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common';
 import {Patient, PatientSchema} from './schemas/patient.schema'
+import {Doctor, DoctorSchema} from './schemas/doctor.schema'
 import {MongooseModule} from '@nestjs/mongoose'
 import { PatientService } from './services/patient.service';
 import { PatientController } from './controllers/patient.controller';
+import { DoctorService } from './services/doctor.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Patient.name, schema: PatientSchema},
+      {name: Doctor.name, schema: DoctorSchema}
     ])
   ],
-  providers: [PatientService],
+  providers: [PatientService, DoctorService],
   controllers: [PatientController],
   exports: [PatientService]
 })
