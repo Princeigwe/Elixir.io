f621944ea5c2a5fac40dfae94e36a1d537a8c836
Prince Igwe <igwep297@gmail.com>
2022-09-01 06:25:17 +0100

    wrote service test to mock Patient mongo Model

--- backend/src/profiles/services/patient.service.spec.ts
+++ backend/src/profiles/services/patient.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PatientService } from './patient.service';
+import {getModelToken} from '@nestjs/mongoose'
 
 describe('PatientService', () => {
   let service: PatientService;
 
+  function mockPatientModel(dto:any) {
+    this.data = dto;
+    this.save  = () => { return this.data }
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PatientService],
+      providers: [
+        PatientService,
+        {
+          provide: getModelToken('Patient'),
+          useValue: mockPatientModel
+        }
+      ],
     }).compile();
 
     service = module.get<PatientService>(PatientService);
