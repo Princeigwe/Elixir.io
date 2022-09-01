613b2b1e403c2f9454b14d499158c04f69b0d682
Prince Igwe <igwep297@gmail.com>
2022-09-01 19:51:56 +0100

    defined mock service and mock model for doctor testing

--- backend/src/profiles/controllers/doctor.controller.spec.ts
+++ backend/src/profiles/controllers/doctor.controller.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DoctorController } from './doctor.controller';
+import {DoctorService} from '../services/doctor.service'
 
 describe('DoctorController', () => {
   let controller: DoctorController;
 
+  let mockDoctorService = {}
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DoctorController],
+      providers: [
+        {
+          provide: DoctorService,
+          useValue: mockDoctorService
+        }
+      ]
     }).compile();
 
     controller = module.get<DoctorController>(DoctorController);
--- backend/src/profiles/services/doctor.service.spec.ts
+++ backend/src/profiles/services/doctor.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DoctorService } from './doctor.service';
+import {getModelToken} from '@nestjs/mongoose'
 
 describe('DoctorService', () => {
   let service: DoctorService;
 
+  function mockDoctorModel (dto:any) {
+    this.data = dto
+    this.save = () => {return this.data}
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DoctorService],
+      providers: [
+        DoctorService,
+        {
+          provide: getModelToken('Doctor'),
+          useValue: mockDoctorModel
+        }
+      ],
     }).compile();
 
     service = module.get<DoctorService>(DoctorService);
