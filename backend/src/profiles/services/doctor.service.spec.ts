4ce293f739a5c7ea1b2e019e96d5f9ffa0e6c0b6
Prince Igwe <igwep297@gmail.com>
2022-09-14 00:17:37 +0100

    mocked usersService dependency

--- backend/src/profiles/services/doctor.service.spec.ts
+++ backend/src/profiles/services/doctor.service.spec.ts
@@ -1,9 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DoctorService } from './doctor.service';
 import {getModelToken} from '@nestjs/mongoose'
+import {UsersService} from '../../users/users.service'
+
 
 describe('DoctorService', () => {
   let service: DoctorService;
+
+  let mockUsersService = {}
 
   function mockDoctorModel (dto:any) {
     this.data = dto
@@ -17,6 +21,10 @@ describe('DoctorService', () => {
         {
           provide: getModelToken('Doctor'),
           useValue: mockDoctorModel
+        },
+        { 
+          provide: UsersService,
+          useValue: mockUsersService
         }
       ],
     }).compile();
