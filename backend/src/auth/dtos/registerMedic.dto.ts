cf1f9bdca5fbce95923ae1f39e1e6dde17869a8f
Prince Igwe <igwep297@gmail.com>
2022-09-14 06:48:59 +0100

    body data for swagger

--- backend/src/auth/dtos/registerMedic.dto.ts
+++ backend/src/auth/dtos/registerMedic.dto.ts
@@ -1,22 +1,27 @@
 import {IsEnum, IsNotEmpty, IsOptional, IsString} from 'class-validator'
 import {DoctorHierarchy} from '../../enums/doctor.hierarchy.enum'
 import {MedicalDepartments} from '../../enums/medical.department.enum'
+import { ApiProperty } from '@nestjs/swagger';
 
 // dto for user and admin registration
 export class RegisterUserConsultantDto {
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     firstName: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     lastName: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     password: string;
@@ -26,10 +31,12 @@ export class RegisterUserConsultantDto {
 
 export class RegisterUserDoctorDto {
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     firstName: string;
@@ -38,14 +45,19 @@ export class RegisterUserDoctorDto {
     @IsString()
     lastName: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     password: string;
 
+    @ApiProperty()
     @IsEnum(MedicalDepartments)
     @IsNotEmpty()
     department: MedicalDepartments
 
+    @ApiProperty({
+        description: " ['Medical Student','Junior Doctor', 'Associate Specialist'] .Giving data for hierarchy is optional, which sets default to Associate Specialist"
+    })
     @IsOptional()
     @IsEnum(DoctorHierarchy)
     hierarchy: DoctorHierarchy
--- backend/src/auth/dtos/registerUser.dto.ts
+++ backend/src/auth/dtos/registerUser.dto.ts
@@ -1,12 +1,15 @@
 import {IsNotEmpty, IsString} from 'class-validator'
+import {ApiProperty} from '@nestjs/swagger'
 
 // dto for user and admin registration
 export class RegisterUserAdminDto {
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     password: string;
