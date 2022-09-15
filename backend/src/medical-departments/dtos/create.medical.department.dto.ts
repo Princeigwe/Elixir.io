81a97fa8f08a4fed18d8a917088eae311b9bb7fa
Prince Igwe <igwep297@gmail.com>
2022-09-15 02:06:12 +0100

    updated swagger docs for medical departments

--- backend/src/medical-departments/dtos/create.medical.department.dto.ts
+++ backend/src/medical-departments/dtos/create.medical.department.dto.ts
@@ -1,8 +1,10 @@
 import {IsEnum, IsNotEmpty, MaxLength} from 'class-validator'
 import {MedicalDepartments} from '../../enums/medical.department.enum'
+import {ApiProperty} from '@nestjs/swagger'
 
 export class CreateMedicalDepartmentDto {
 
+    @ApiProperty({ description: " [ Cardiology, Dermatology, Urology, IntensiveCareMedicine, Neurology, Surgery, Radiology, Pharmacy] "})
     @IsEnum(MedicalDepartments)
     @IsNotEmpty()
     name: MedicalDepartments;
