d048ef5e86309d61651b625ab249bd7dfb117368
Prince Igwe <igwep297@gmail.com>
2022-09-15 01:58:58 +0100

    swagger doc for medical department

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle("Elixir.io")
-    .setDescription("A health tech API for Elixir.io")
+    .setDescription("A health-tech API for Elixir.io")
     .setVersion('1.0')
     .build()
   
--- backend/src/medical-departments/medical-departments.controller.ts
+++ backend/src/medical-departments/medical-departments.controller.ts
@@ -5,12 +5,27 @@ import {RolesGuard} from '../roles.guard'
 import {Roles} from '../roles.decorator'
 import { Role } from '../enums/role.enum';
 import {CreateMedicalDepartmentDto} from './dtos/create.medical.department.dto'
+import {ApiTags, ApiResponse, ApiOperation, ApiParam, ApiQuery} from '@nestjs/swagger'
 
 
+@ApiTags('Medical Departments')
 @Controller('medical-departments')
 export class MedicalDepartmentsController {
     constructor(private medicalDepartmentsService: MedicalDepartmentsService) {}
 
+    @ApiOperation({description: "Creates a new department with one of [ Cardiology, Dermatology, Urology, IntensiveCareMedicine, Neurology, Surgery, Radiology, Pharmacy] as name data, only by an admin"})
+    @ApiResponse({ 
+        status: 201,
+        description: "Returns the object of a Medical Department."
+    })
+    @ApiResponse({ 
+        status: 401,
+        description: "Unauthorized"
+    })
+    @ApiResponse({ 
+        status: 400,
+        description: "Medical department already exists, create with a different name"
+    })
     @UseGuards(JwtAuthGuard, RolesGuard)
     @Post()
     @Roles(Role.Admin)
@@ -18,6 +33,12 @@ export class MedicalDepartmentsController {
         return this.medicalDepartmentsService.createMedicalDepartment(body.name)
     }
 
+    @ApiOperation({description: "Get medical departments or queries endpoint for one"})
+    @ApiQuery({ 
+        name: 'name',
+        required: false,
+        description: "This is the query key to get department by name",
+    })
     @Get()
     async getOrSearchMedicalDepartments(@Query('name') name?: string) { // name is optional in order to pass test
         if(name) {
@@ -26,6 +47,12 @@ export class MedicalDepartmentsController {
         return this.medicalDepartmentsService.getMedicalDepartments()
     }
 
+    @ApiOperation({description: "DELETE departments or one by name query, executed by an admin"})
+    @ApiQuery({ 
+        name: 'name',
+        required: false,
+        description: "Deletes a department by name"
+    })
     @UseGuards(JwtAuthGuard, RolesGuard)
     @Delete()
     @Roles(Role.Admin)
