b62bcbfd3f7639934f8ccfcceeff646c5b00c5d9
Prince Igwe <igwep297@gmail.com>
2023-01-08 05:57:04 +0100

    mongodump runs, but can't find the dump folder

--- backend/src/bash_scripts/datarestore.sh
+++ backend/src/bash_scripts/datarestore.sh
@@ -22,6 +22,7 @@ mongo --version
 
 # cp -u -r  mongo-volume/*  mongo-backup-volume/
 
+# mongodump --uri="mongodb://elixir_io:elixir_io@localhost:27107/elixir_io?ssl=false&authSource=admin"
+# mongodump --user=elixir_io --db=elixir_io --password=elixir_io --authenticationDatabase=admin
 
-mongodump --uri="mongodb://elixir_io:elixir_io@localhost:27107/elixir_io?ssl=false&authSource=admin"
-
+mongodump --host elixir_mongodb --port 27017 --username elixir_io --db elixir_io --password elixir_io --authenticationDatabase admin --out ./
