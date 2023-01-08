d91cf7e77b8726692504f4b9b091aa4a64e2979a
Prince Igwe <igwep297@gmail.com>
2023-01-09 00:42:32 +0100

    data restoration from databse backup' esit exit

--- backend/src/bash_scripts/datarestore.sh
+++ backend/src/bash_scripts/datarestore.sh
@@ -6,25 +6,14 @@ pwd
 
 cd src
 
-pwd
-
-ls
-
-node --version
-
-mongo --version
-
-# mongodump
-
-# node scripts/mongodump.ts
-# node scripts/mongorestore.ts
+#PROCESS OF RESTORING DATA FROM ELIXIR_MONGODB_BACKUP TO ELIXIR_MONGODB
 
 
-# cp -u -r  mongo-volume/*  mongo-backup-volume/
+# mongodump --host elixir_mongodb --port 27017 --username elixir_io --db elixir_io --password elixir_io --authenticationDatabase admin --archive=elixir.archive
 
-# mongodump --uri="mongodb://elixir_io:elixir_io@localhost:27107/elixir_io?ssl=false&authSource=admin"
-# mongodump --user=elixir_io --db=elixir_io --password=elixir_io --authenticationDatabase=admin
+# dump the data of the database elixir_io_backup(docker container name) in to an archive file called elixir.archive
+mongodump --host elixir_mongodb_backup --port 27021 --username elixir_io  --password elixir_io --authenticationDatabase admin --archive=elixir.archive
 
-# mongodump --host elixir_mongodb --port 27017 --username elixir_io --db elixir_io --password elixir_io --authenticationDatabase admin --out ./dump/
-mongodump --host elixir_mongodb --port 27017 --username elixir_io --db elixir_io --password elixir_io --authenticationDatabase admin --archive=elixir.archive
 
+# transfer the data of the archive file to database with "elixir_mongodb" as host
+mongorestore --host elixir_mongodb --port 27017 --username elixir_io  --password elixir_io --authenticationDatabase admin --archive=elixir.archive
