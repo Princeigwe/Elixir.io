a9fd20dc93ddf60356d9001f48a741e5549ea0aa
Prince Igwe <igwep297@gmail.com>
2023-01-24 13:18:02 +0100

    modified the casl factory for create permission

--- backend/src/casl/casl-ability.factory.ts
+++ backend/src/casl/casl-ability.factory.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@nestjs/common'
 import {User} from '../users/users.schema'
 import {Doctor} from '../profiles/schemas/doctor.schema'
 import {Patient} from '../profiles/schemas/patient.schema'
+import { MedicalRecord } from '../electronic-health-records/schemas/medical.record.schema'
 import { InferSubjects, Ability, AbilityBuilder, AbilityClass, ExtractSubjectType} from '@casl/ability'
 import {Action} from '../enums/action.enum'
 
@@ -12,7 +13,7 @@ export class CaslAbilityFactory {
 
     createForUser(user: User) {
         const {can, build} = new AbilityBuilder(
-            Ability as AbilityClass< Ability<[Action, InferSubjects<typeof Doctor | typeof Patient | typeof User> | 'all']> >
+            Ability as AbilityClass< Ability<[Action, InferSubjects<typeof Doctor | typeof Patient | typeof User | typeof MedicalRecord> | 'all']> >
 
         )
 
@@ -23,9 +24,11 @@ export class CaslAbilityFactory {
         // ** using the Doctor model here doesn't work, so 'all' is used to find any subject with an email attribute to be compared to user.email **
         can(Action.Update, 'all', {email: user.email})
 
+        can(Action.Create, 'all', {email: user.email})
+
 
         return build({
-            detectSubjectType: (item) => item.constructor as ExtractSubjectType<InferSubjects<typeof Doctor | typeof Patient | typeof User> | 'all'>
+            detectSubjectType: (item) => item.constructor as ExtractSubjectType<InferSubjects<typeof Doctor | typeof Patient | typeof User | typeof MedicalRecord> | 'all'>
         })
     }
 }
