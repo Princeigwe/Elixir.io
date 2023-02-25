2e755987f5f4ea7558f1c28ac0c1d972f3b4ba7d
Prince Igwe <igwep297@gmail.com>
2023-02-26 00:00:00 +0100

    modified the add prescription method to encrypt data

--- backend/src/electronic-health-records/services/prescription.service.ts
+++ backend/src/electronic-health-records/services/prescription.service.ts
@@ -6,7 +6,10 @@ import { MedicalRecordService } from './medical-record.service';
 import { User } from '../../users/users.schema';
 import { RouteOfAdministration } from '../../enums/route.of.administration.enum';
 import { DoctorService } from '../../profiles/services/doctor.service';
+import * as AesEncryption from 'aes-encryption'
 
+const aes = new AesEncryption()
+aes.setSecretKey(process.env.ENCRYPTION_KEY)
 
 type medication = {
     name: string,
@@ -29,6 +32,15 @@ export class PrescriptionService {
     /* this allows medical providers create prescriptions to medical records they have access to.
         The user parameter here refers to that of the medical provider
     */
+    /**
+     * The function takes in a medical record id, a user, an array of medications, and a string of
+     * instructions. It then creates a prescription and adds it to the medical record
+     * @param {string} record_id - the id of the medical record that the prescription is being added to
+     * @param {User} user - User - this is the user object that is passed in from the controller.
+     * @param {medication[]} medications - medication[]
+     * @param {string} instructions - string
+     * @returns The prescription is being returned.
+     */
     async addPrescriptionToMedicalRecord(record_id: string, user: User, medications: medication[], instructions: string) {
         const medicalRecord = await this.medicalRecordService.readActionOfMedicalRecordByMedicalProvider(record_id, user)
         const medicalProvider = await this.doctorService.getDoctorProfileByEmail(user.email)
@@ -36,24 +48,38 @@ export class PrescriptionService {
         const prescription = await this.prescriptionModel.create({
             medicalRecord: medicalRecord._id,
             patient_demographics: {
-                firstName: medicalRecord.patient_demographics.firstName,
-                lastName:  medicalRecord.patient_demographics.lastName,
-                email:     medicalRecord.patient_demographics.email,
+                firstName: aes.encrypt(medicalRecord.patient_demographics.firstName),
+                lastName:  aes.encrypt(medicalRecord.patient_demographics.lastName),
+                email:     aes.encrypt(medicalRecord.patient_demographics.email),
                 age:       medicalRecord.patient_demographics.age,
-                address:   medicalRecord.patient_demographics.address,
-                telephone: medicalRecord.patient_demographics.telephone,
+                address:   aes.encrypt(medicalRecord.patient_demographics.address),
+                telephone: aes.encrypt(medicalRecord.patient_demographics.telephone),
             }, 
             prescriber: {
-                doctor_firstName:  medicalProvider.firstName,
-                doctor_lastName:   medicalProvider.lastName,
-                doctor_department: medicalProvider.department,
-                doctor_email:      medicalProvider.email,
-                doctor_telephone:  medicalProvider.telephone,
+                doctor_firstName:  aes.encrypt(medicalProvider.firstName),
+                doctor_lastName:   aes.encrypt(medicalProvider.lastName),
+                doctor_department: aes.encrypt(medicalProvider.department),
+                doctor_email:      aes.encrypt(medicalProvider.email),
+                doctor_telephone:  aes.encrypt(medicalProvider.telephone),
             },
-            instructions: instructions
+            instructions: aes.encrypt(instructions)
         })
 
-        medications.forEach( (medication) => { prescription.medications.push(medication) } )
+        medications.forEach( (medication) => { 
+
+            medication.dosage                = aes.encrypt(medication.dosage)
+            medication.duration              = aes.encrypt(medication.duration)
+            medication.frequency             = aes.encrypt(medication.frequency)
+            medication.name                  = aes.encrypt(medication.name)
+            medication.routeOfAdministration = aes.encrypt(medication.routeOfAdministration)
+
+            prescription.medications.push(medication) 
+        } )
         return prescription.save()
+    }
+
+    // * this action will be performed by an administrator to read all medical prescriptions
+    async getPrescriptions() {
+
     }
 }
