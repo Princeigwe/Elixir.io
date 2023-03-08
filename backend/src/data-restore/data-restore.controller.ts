8e60bbb381730d8a7e30d64fb86a7d910be77e12
Prince Igwe <igwep297@gmail.com>
2023-03-08 02:22:28 +0100

    swagger docs for data restoration endpoint

--- backend/src/data-restore/data-restore.controller.ts
+++ backend/src/data-restore/data-restore.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import {DataRestoreService} from './data-restore.service'
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags("Data Restoration")
 @Controller('data-restore')
 export class DataRestoreController {
     constructor(private dataRestoreService: DataRestoreService) {}
 
+    @ApiOperation({description: "This endpoint restores data from the backup database in case of data loss"})
     @Get()
     async dataRestore() {
         return this.dataRestoreService.dataRestore()
