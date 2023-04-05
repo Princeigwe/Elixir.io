e6794564662bd2d19191eead2ef60bb182a4ed7d
Prince Igwe <igwep297@gmail.com>
2023-04-05 02:11:41 +0100

    mocked entities in token service test file

--- backend/src/token/token.service.spec.ts
+++ backend/src/token/token.service.spec.ts
@@ -1,12 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TokenService } from './token.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { UsersService } from '../users/users.service';
+import { AuthService } from '../auth/auth.service';
+
 
 describe('TokenService', () => {
   let service: TokenService;
 
+  function mockResetTokenModel (dto:any) {
+    this.data = dto
+    this.save = () => {return this.data}
+  }
+
+  let mockUserService = {}
+  let mockAuthService = {}
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TokenService],
+      providers: [
+        TokenService,
+        {provide: getModelToken('ResetToken'), useValue: mockResetTokenModel},
+        { 
+          provide: AuthService,
+          useValue: mockAuthService
+        },
+        { provide: UsersService, 
+          useValue: mockUserService
+        }
+      ],
     }).compile();
 
     service = module.get<TokenService>(TokenService);
