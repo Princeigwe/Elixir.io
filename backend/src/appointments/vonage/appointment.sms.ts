020a90d1e7e691d3525b84fd4374916493ddbcd9
Prince Igwe <igwep297@gmail.com>
2023-05-08 18:21:50 +0100

    updated code to send sms on appointment reschedule... still yet to be tested because of the parties profile telephone problem

--- backend/src/appointments/vonage/appointment.sms.ts
+++ backend/src/appointments/vonage/appointment.sms.ts
@@ -8,10 +8,30 @@ const vonage = new Vonage({
 
 export class VonageSMS {
 
-    async sendScheduleMessage(doctorMobileNumber: string, patientName: string, date: Date) {
+    async sendScheduleMessage(doctorTelephone: string, patientName: string, date: Date) {
         const from = process.env.VONAGE_BRAND_NAME
-        const to = doctorMobileNumber
+        const to = doctorTelephone
         const text = `You have a new appointment scheduled with ${patientName} on ${date}.`
+
+        await vonage.sms.send({to, from, text})
+        .then(resp => { console.log('Message sent successfully'); console.log(resp); })
+        .catch(err => { console.log('There was an error sending the messages.'); console.error(err); });
+    }
+
+    async sendRescheduleMessageByPatient(doctorTelephone: string, patientName: string, date: Date) {
+        const from = process.env.VONAGE_BRAND_NAME
+        const to = doctorTelephone
+        const text = `The appointment with ${patientName} is now rescheduled to ${date}.`
+
+        await vonage.sms.send({to, from, text})
+        .then(resp => { console.log('Message sent successfully'); console.log(resp); })
+        .catch(err => { console.log('There was an error sending the messages.'); console.error(err); });
+    }
+
+    async sendRescheduleMessageByMedicalProvider(patientTelephone: string, doctorName: string, date: Date) {
+        const from = process.env.VONAGE_BRAND_NAME
+        const to = patientTelephone
+        const text = `The appointment with your doctor, ${doctorName}, has been rescheduled to ${date}.`
 
         await vonage.sms.send({to, from, text})
         .then(resp => { console.log('Message sent successfully'); console.log(resp); })
