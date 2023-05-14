ba7ce37ed1a79457723ed0af216dbcf87fd600cf
Prince Igwe <igwep297@gmail.com>
2023-05-14 21:28:24 +0100

    added code for creating session

--- backend/src/stream-call/stream-call.controller.ts
+++ backend/src/stream-call/stream-call.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Render, Get } from '@nestjs/common';
+import { Controller, Render, Get, Post, Body } from '@nestjs/common';
+import {CreateSessionDto } from './dtos/create.session.dto'
+import {StreamCallService} from './stream-call.service'
 
 @Controller('stream-call')
 export class StreamCallController {
 
+    constructor(
+        private streamCallService: StreamCallService
+    ) {}
+
     @Get()
     @Render('stream_call')
     async openStreamCallPage() {}
+
+    @Post()
+    async createSession(@Body() body: CreateSessionDto) {
+        return await this.streamCallService.createSession(body.patientEmail, body.doctorEmail)
+    }
 
 }
--- backend/src/stream-call/stream-call.service.ts
+++ backend/src/stream-call/stream-call.service.ts
@@ -15,9 +15,10 @@ export class StreamCallService {
         @InjectModel(Session.name) private sessionModel: Model<SessionDocument>
     ) {}
 
+
     /**
-     * This function creates a session for a patient and a doctor using OpenTok and returns a Promise
-     * that resolves to a SessionDocument.
+     * This function creates a new session for a patient and a doctor using OpenTok and saves it to a
+     * database.
      * @param {string} patientEmail - A string representing the email address of the patient who is
      * participating in the session.
      * @param {string} doctorEmail - The email address of the doctor who will be participating in the
@@ -28,13 +29,17 @@ export class StreamCallService {
         return new Promise<SessionDocument>((resolve, reject) => {
             opentok.createSession({ mediaMode: "routed" }, async (error, session) => {
                 if (error) {
-                    console.log("Error creating session:", error);
-                    reject(error);
-                } else {
+                console.log("Error creating session:", error);
+                reject(error);
+                } 
+                else {
                     try {
-                        const sessionRoom = await this.sessionModel.create({ sessionId: session.sessionId, patientEmail, doctorEmail });
+                        const sessionRoom = await this.sessionModel.create({ sessionID: session.sessionId, patientEmail, doctorEmail });
+                        await sessionRoom.save();
+                        console.log(sessionRoom);
                         resolve(sessionRoom);
                     } catch (error) {
+                        console.log(error);
                         reject(error);
                     }
                 }
