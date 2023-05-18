347e283f2a69dc56d25c057befa77b0ec8ed5cea
Prince Igwe <igwep297@gmail.com>
2023-05-18 15:06:22 +0100

    removed the forwardref dependency attached to streamcallService for circular dependency

--- backend/src/appointments/appointments.service.ts
+++ backend/src/appointments/appointments.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Inject, forwardRef } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Appointment, AppointmentDocument } from './appointment.schema';
@@ -62,6 +62,7 @@ export class AppointmentsService {
 
         const patientName = `${patientProfile.firstName} ${patientProfile.lastName}`
 
+        //todo: uncomment this when done with writing stream call code
         // send sms notification to the patient assigned doctor, notifying them of the scheduled appointment
         // await vonageSMS.sendScheduleMessage( assignedDoctorProfile.telephone, patientName, appointment.date )
 
@@ -149,22 +150,23 @@ export class AppointmentsService {
         
         const updatedAppointment = await this.appointmentModel.findById(appointment_id)
 
+        //todo: uncomment this when done with writing stream call code
         // send sms notification to the patient, notifying them of the confirmed appointment
         // await vonageSMS.sendAppointmentConfirmationMessage(patient.telephone, doctorName, updatedAppointment.date)
 
-        await this.createStreamCallSessionAndNotifyPartiesInvolved(appointment.patient.email, user.email)
+        await this.createStreamCallSessionAndNotifyPartiesInvolved(appointment.patient.email, user.email, appointment._id)
 
         return updatedAppointment
     }
 
 
-    async createStreamCallSessionAndNotifyPartiesInvolved(patientEmail: string, doctorEmail: string) {
+    async createStreamCallSessionAndNotifyPartiesInvolved(patientEmail: string, doctorEmail: string, appointment_id: string) {
         const patient = await this.patientService.getPatientByEmailForAppointment(patientEmail)
         const doctorName = `${patient.assignedDoctor.name}`
         const patientName = `${patient.firstName} ${patient.lastName}`
 
         // create a stream call session
-        const session = await this.streamCallService.createSession(patientEmail, doctorEmail)
+        const session = await this.streamCallService.createSession(patientEmail, doctorEmail, appointment_id)
 
         // create a token for the doctor of the stream call session
         const doctorToken = await this.streamCallService.generateToken(session.sessionID)
@@ -275,6 +277,15 @@ export class AppointmentsService {
             return myAppointment
         }
     }
+
+
+    // async getAppointmentForStreamCallTokenGeneration(appointment_id: string) {
+    //     const appointment = await this.appointmentModel.findById(appointment_id).exec()
+    //         if(!appointment) {
+    //             throw new HttpException("This appointment does not exist", HttpStatus.NOT_FOUND)
+    //         }
+    //         return appointment
+    // }
 
     // by admin
     async clearAppointments() {
