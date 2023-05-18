a03a992e18ead2bb249fed91219de98abc5e6d94
Prince Igwe <igwep297@gmail.com>
2023-05-18 15:11:49 +0100

    reference appointment document for which the session will be created, in order to get the patient and doctor emails

--- backend/src/stream-call/session.schema.ts
+++ backend/src/stream-call/session.schema.ts
@@ -1,5 +1,8 @@
 import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
 import {Document} from 'mongoose'
+import { Appointment } from "../appointments/appointment.schema";
+import * as mongoose from 'mongoose'
+
 
 export type SessionDocument = Session & Document
 
@@ -14,6 +17,9 @@ export class Session {
 
     @Prop()
     doctorEmail: string;
+
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'Appointment'})
+    appointment: Appointment
 }
 
 export const SessionSchema = SchemaFactory.createForClass(Session)
