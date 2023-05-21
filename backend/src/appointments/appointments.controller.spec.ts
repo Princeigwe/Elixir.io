babc6d2874064b704074d6fc41e7e590e4c051e1
Prince Igwe <igwep297@gmail.com>
2023-05-21 04:45:30 +0100

    passed a dependency test definitions

--- backend/src/appointments/appointments.controller.spec.ts
+++ backend/src/appointments/appointments.controller.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppointmentsController } from './appointments.controller';
+import { AppointmentsService } from './appointments.service';
 
 describe('AppointmentsController', () => {
   let controller: AppointmentsController;
 
+  let mockAppointmentsService = {}
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AppointmentsController],
+      providers: [
+        {
+          provide: AppointmentsService, useValue: mockAppointmentsService
+        }
+      ]
     }).compile();
 
     controller = module.get<AppointmentsController>(AppointmentsController);
