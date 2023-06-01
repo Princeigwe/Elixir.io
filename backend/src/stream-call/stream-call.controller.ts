44006a784d36a834dc50093f7eb9c225f41bca79
Prince Igwe <igwep297@gmail.com>
2023-06-01 06:23:13 +0100

    added endpoint to configure daily domain by admin

--- backend/src/stream-call/stream-call.controller.ts
+++ backend/src/stream-call/stream-call.controller.ts
@@ -2,18 +2,34 @@ import { Controller, Render, Get, Post, Body, Request, UseGuards, Param, HttpExc
 import {CreateSessionDto } from './dtos/create.session.dto'
 import {StreamCallService} from './stream-call.service'
 import {JwtAuthGuard} from '../auth/guards/jwt-auth.guard'
+import { ConfigDailyDomainDto } from './dtos/config.Daily.domain.dto';
+import {Roles} from '../roles.decorator'
+import {Role} from '../enums/role.enum'
+import { RolesGuard } from '../roles.guard';
 
 @Controller('stream-call')
 export class StreamCallController {
 
     constructor(
-        // private streamCallService: StreamCallService
+        private streamCallService: StreamCallService
     ) {}
 
 
     @Get('/:cipher/:key/:code')
     @Render('stream_call')
     async streamCall( ) {}
+
+    @Get('/agora')
+    @Render('stream_call_agora')
+    async streamCallAgora( ) {}
+
+
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Post('configure-daily-domain')
+    @Roles(Role.Admin)
+    async configureDailyDomainByAdmin(@Body() body: ConfigDailyDomainDto) {
+        return await this.streamCallService.configureDailyDomainByAdmin(body.enable_advanced_chat, body.enable_people_ui)
+    }
 
 
     // @Get('/:sessionID')
